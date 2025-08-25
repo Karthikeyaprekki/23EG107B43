fdb220f5bd97e8a4055c2084bc0d5a0d8321db2f
Preethi <23eg107b23@anurag.edu.in>
2025-08-25 21:37:47 +0530

    updated app_server,controllers,others,routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'preethi' }); 
+  res.render('index', { title: 'ravi' }); 
 });
 
 module.exports = router;
