7ff8476d98c836dceeea3d6ce636aef2888ba1dd
Preethi <23eg107b23@anurag.edu.in>
2025-10-12 17:43:49 +0530

    Initial project upload

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 
 var app = express();
-
+require('./app_server/models/db');
 // view engine setup
 app.set('views', path.join(__dirname,'app_server', 'views'));
 app.set('view engine', 'jade');
@@ -32,6 +32,16 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+  app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    title: 'Error',       // ✅ Add this
+    message: err.message,
+    error: err
+  });
+});
+
 
   // render the error page
   res.status(err.status || 500);
@@ -39,3 +49,8 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+
+
+
